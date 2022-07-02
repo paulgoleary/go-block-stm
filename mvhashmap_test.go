a84e5a726020bc1500d7605e22d9305a49f78025
Pratik Patil <pratikspatil024@gmail.com>
2022-07-02 10:35:35 +0530

    added benmchmark + normal tests

--- mvhashmap_test.go
+++ mvhashmap_test.go
@@ -2,26 +2,174 @@ package block_stm
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/require"
+	"math/rand"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
+// create data for a given txIdx and incarnation
 func valueFor(txIdx, inc int) []byte {
 	return []byte(fmt.Sprintf("%ver:%ver:%ver", txIdx*5, txIdx+inc, inc*5))
 }
 
-func TestMVHashMapBasics(t *testing.T) {
+// Result of the following benchmark test:
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/paulgoleary/go-block-stm
+// BenchmarkWriteTimeSameLocationDifferentTxIdx-8   	       1	1383456542 ns/op	256013944 B/op	10000068 allocs/op
+// PASS
+// ok  	github.com/paulgoleary/go-block-stm	1.793s
+func BenchmarkWriteTimeSameLocationDifferentTxIdx(b *testing.B) {
+	mvh2 := MakeMVHashMap()
+	ap2 := []byte("/foo/b")
+	b.ResetTimer()
+	for i := 0; i < 1000000; i++ {
+		txIdx := rand.Intn(1000000000000000)
+		mvh2.Write(ap2, Version{txIdx, 1}, valueFor(txIdx, 1))
+	}
+}
 
+// Result of the following benchmark test:
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/paulgoleary/go-block-stm
+// BenchmarkReadTimeSameLocationDifferentTxIdx-8   	1000000000	         0.8775 ns/op	       0 B/op	       0 allocs/op
+// PASS
+// ok  	github.com/paulgoleary/go-block-stm	156.475s
+func BenchmarkReadTimeSameLocationDifferentTxIdx(b *testing.B) {
+	mvh2 := MakeMVHashMap()
+	ap2 := []byte("/foo/b")
+	txIdxSlice := []int{}
+	for i := 0; i < 1000000; i++ {
+		txIdx := rand.Intn(1000000000000000)
+		txIdxSlice = append(txIdxSlice, txIdx)
+		mvh2.Write(ap2, Version{txIdx, 1}, valueFor(txIdx, 1))
+	}
+
+	b.ResetTimer()
+	for _, value := range txIdxSlice {
+		mvh2.Read(ap2, value)
+	}
+}
+
+// go test -run TestLowerIncarnation -v
+// this will panic
+// PSP - handel panic
+func TestLowerIncarnation(t *testing.T) {
+	ap1 := []byte("/foo/b")
+
+	mvh := MakeMVHashMap()
+
+	mvh.Write(ap1, Version{0, 2}, valueFor(0, 2))
+	mvh.Read(ap1, 0)
+	mvh.Write(ap1, Version{1, 2}, valueFor(1, 2))
+	mvh.Write(ap1, Version{0, 5}, valueFor(0, 5))
+	mvh.Write(ap1, Version{1, 5}, valueFor(1, 5))
+	// will fail (panic) as Version{0 4} has lower incarnation than Version{0 5}
+	mvh.Write(ap1, Version{0, 4}, valueFor(0, 4))
+}
+
+func TestMarkEstimate(t *testing.T) {
+	ap1 := []byte("/foo/b")
+
+	mvh := MakeMVHashMap()
+
+	mvh.Write(ap1, Version{7, 2}, valueFor(7, 2))
+	mvh.MarkEstimate(ap1, 7)
+	mvh.Write(ap1, Version{7, 4}, valueFor(7, 4))
+}
+
+func TestTimeComplexity(t *testing.T) {
+
+	// for 1000000 read and write with no dependency at different memory location
+	// takes around 1.1 - 1.2 seconds
+	// only Write: 1 second
+	// only Read: 0.23 seconds (as it is not reaching the Floor function)
+	mvh1 := MakeMVHashMap()
+	for i := 0; i < 1000000; i++ {
+		ap1 := []byte(fmt.Sprint(i))
+		mvh1.Write(ap1, Version{i, 1}, valueFor(i, 1))
+		mvh1.Read(ap1, i)
+	}
+	// fmt.Println("\nMVHashMap:", "\n ", mvh1)
+
+	// for 1000000 read and write with dependency at same memory location
+	// takes around 1.1 - 1.2 seconds
+	// only Write: 0.8 seconds
+	// only Read: 0.1 - 0.2 seconds (as it is not reaching the Floor function)
+	mvh2 := MakeMVHashMap()
+	ap2 := []byte("/foo/b")
+	for i := 0; i < 1000000; i++ {
+		mvh2.Write(ap2, Version{i, 1}, valueFor(i, 1))
+		mvh2.Read(ap2, i)
+	}
+	// fmt.Println("\nMVHashMap:", "\n ", mvh2)
+
+}
+
+// around 0.85 seconds
+func TestWriteTimeSameLocationDifferentTxnIdx(t *testing.T) {
+	mvh1 := MakeMVHashMap()
+	ap1 := []byte("/foo/b")
+	for i := 0; i < 1000000; i++ {
+		mvh1.Write(ap1, Version{i, 1}, valueFor(i, 1))
+	}
+	// fmt.Println("\nMVHashMap:", "\n ", mvh2)
+}
+
+// around 0.35 seconds
+func TestWriteTimeSameLocationSameTxnIdx(t *testing.T) {
+	mvh1 := MakeMVHashMap()
+	ap1 := []byte("/foo/b")
+	for i := 0; i < 1000000; i++ {
+		mvh1.Write(ap1, Version{1, i}, valueFor(i, 1))
+	}
+	// fmt.Println("\nMVHashMap:", "\n ", mvh2)
+}
+
+// around 1.0 seconds
+func TestWriteTimeDifferentLocation(t *testing.T) {
+	mvh1 := MakeMVHashMap()
+	for i := 0; i < 1000000; i++ {
+		ap1 := []byte(fmt.Sprint(i))
+		mvh1.Write(ap1, Version{i, 1}, valueFor(i, 1))
+	}
+	// fmt.Println("\nMVHashMap:", "\n ", mvh2)
+}
+
+// around 0.18 seconds
+func TestReadTimeSameLocation(t *testing.T) {
+	mvh1 := MakeMVHashMap()
+	ap1 := []byte("/foo/b")
+	mvh1.Write(ap1, Version{1, 1}, valueFor(1, 1))
+	for i := 0; i < 1000000; i++ {
+		mvh1.Read(ap1, 2)
+	}
+	// fmt.Println("\nMVHashMap:", "\n ", mvh1)
+}
+
+// to view logs, run test with:
+// go test -run TestMVHashMapBasics -v
+func TestMVHashMapBasics(t *testing.T) {
+	fmt.Println("\nIn TestMVHashMapBasics")
+
+	// t.Logf("Hello")
+	fmt.Println("Hello Again!")
+
+	// memory locations
 	ap1 := []byte("/foo/b")
 	ap2 := []byte("/foo/c")
 	ap3 := []byte("/foo/d")
 
 	mvh := MakeMVHashMap()
+	fmt.Println("\nmvh:", mvh)
 
 	res := mvh.Read(ap1, 5)
 	require.Equal(t, -1, res.depIdx)
 
 	mvh.Write(ap1, Version{10, 1}, valueFor(10, 1))
+	fmt.Println("\nmvh:", mvh)
 
 	res = mvh.Read(ap1, 9)
 	require.Equal(t, -1, res.depIdx, "reads that should go the the DB return dependency -1")
@@ -36,7 +184,9 @@ func TestMVHashMapBasics(t *testing.T) {
 
 	// More writes.
 	mvh.Write(ap1, Version{12, 0}, valueFor(12, 0))
+	fmt.Println("\nmvh:", mvh)
 	mvh.Write(ap1, Version{8, 3}, valueFor(8, 3))
+	fmt.Println("\nmvh:", mvh)
 
 	// Verify reads.
 	res = mvh.Read(ap1, 15)
@@ -64,6 +214,7 @@ func TestMVHashMapBasics(t *testing.T) {
 	// Delete the entry written by 10, write to a different ap.
 	mvh.Delete(ap1, 10)
 	mvh.Write(ap2, Version{10, 2}, valueFor(10, 2))
+	fmt.Println("\nmvh:", mvh)
 
 	// Read by txn 11 no longer observes entry from txn 10.
 	res = mvh.Read(ap1, 11)
@@ -73,7 +224,9 @@ func TestMVHashMapBasics(t *testing.T) {
 
 	// Reads, writes for ap2 and ap3.
 	mvh.Write(ap2, Version{5, 0}, valueFor(5, 0))
+	fmt.Println("\nmvh:", mvh)
 	mvh.Write(ap3, Version{20, 4}, valueFor(20, 4))
+	fmt.Println("\nmvh:", mvh)
 
 	res = mvh.Read(ap2, 10)
 	require.Equal(t, 5, res.depIdx)
@@ -106,4 +259,5 @@ func TestMVHashMapBasics(t *testing.T) {
 	require.Equal(t, 2, res.incarnation)
 	require.Equal(t, valueFor(10, 2), res.value)
 
+	fmt.Println("\nmvh:", mvh)
 }
